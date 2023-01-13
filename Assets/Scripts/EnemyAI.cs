2b16d127d8d152ef4cd99ea4863d47e3be663715
Gutu_Nicolae <nicolausclauss@gmail.com>
2023-01-13 14:08:27 +0200

    Enemy movement

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent navMeshAgent;
+
+    float targetDistance = Mathf.Infinity;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        targetDistance = Vector3.Distance(target.position, transform.position);
+
+        if (targetDistance <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+}
