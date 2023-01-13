9c3e9f5217bc5a39794e6bde47f4290c50f21481
Gutu_Nicolae <nicolausclauss@gmail.com>
2023-01-13 21:27:26 +0200

    Shooting mechanic with raycast added (Bullet projectile and Hitscan methods)

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -8,31 +8,42 @@ using StarterAssets;
 public class ThirdPersonShooterController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private LayerMask aimColliderLayerMask = new();
+
     [SerializeField] private float normalSensitivity;
     [SerializeField] private float aimSensitivity;
-    [SerializeField] private LayerMask aimColliderLayerMask = new();
+
     [SerializeField] private Transform debugTransform;
+    [SerializeField] private Transform bulletPrefab;
+    [SerializeField] private Transform spawnBulletPosition;
+    [SerializeField] private Transform vfxHitGreen;
+    [SerializeField] private Transform vfxHitRed;
 
     private StarterAssetsInputs starterAssetsInputs;
     private ThirdPersonController thirdPersonController;
+    private Animator animator;
 
     private void Awake()
     {
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
         thirdPersonController = GetComponent<ThirdPersonController>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Vector3 mouseWorldPosition = Vector3.zero;
 
-        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Vector2 screenCenterPoint = new(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+
+        Transform hitTransform = null;
 
         if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
         {
             debugTransform.position = raycastHit.point;
             mouseWorldPosition = raycastHit.point;
+            hitTransform = raycastHit.transform;
         }
 
         if (starterAssetsInputs.aim)
@@ -52,6 +63,26 @@ public class ThirdPersonShooterController : MonoBehaviour
             aimVirtualCamera.gameObject.SetActive(false);
             thirdPersonController.SetSensitivity(normalSensitivity);
             thirdPersonController.SetRotateOnMove(true);
+        }
+
+        if (starterAssetsInputs.shoot)
+        {
+            if (hitTransform != null)
+            {
+                if (hitTransform.GetComponent<BulletTarget>() != null)
+                {
+                    //hit target
+                    Instantiate(vfxHitGreen, mouseWorldPosition, Quaternion.identity);
+                }
+                else
+                {
+                    //hit else
+                    Instantiate(vfxHitRed, mouseWorldPosition, Quaternion.identity);
+                }
+            }
+            //Vector3 aimDir = (mouseWorldPosition - spawnBulletPosition.position).normalized;
+            //Instantiate(bulletPrefab, spawnBulletPosition.position, Quaternion.LookRotation(aimDir, Vector3.up));
+            starterAssetsInputs.shoot = false;
         }
 
     }
