c6e4017f50c8ec59e9180b6442845587572d069f
Gutu_Nicolae <nicolausclauss@gmail.com>
2023-01-17 17:11:55 +0200

    Healthbar

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] private float hitPoints = 100f;
+    [SerializeField] private float hitPoints = 100;
+    [SerializeField] private Healthbar healthbar;
 
     private DamageDisplay damageDisplay;
 
     private void Start()
     {
         damageDisplay = GetComponent<DamageDisplay>();
+        healthbar.UpdateHealthbar(100, hitPoints);
     }
 
     public void TakeDamage(float damage)
     {
         hitPoints -= damage;
-
+        healthbar.UpdateHealthbar(100, hitPoints);
         damageDisplay.ShowDamageDisplay();
 
         if (hitPoints <= 0)
         {
+            healthbar.UpdateHealthbar(100, hitPoints);
             BroadcastMessage("HandleDeath");
         }
     }
